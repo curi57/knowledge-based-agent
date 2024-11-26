ef553db71c5112f3ede690370ccc054411373f1d
bitficker <shopper.tiago@gmail.com>
2024-11-26 13:55:57 -0300

    puzzle 3

--- puzzle.py
+++ puzzle.py
@@ -9,13 +9,6 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
-# new symbols to complete knowledge0
-ASaysAKnight = Symbol("A says he's AKnight")
-ASaysAKnave = Symbol("A says he's AKnave")
-ASaysBKnave = Symbol("A says B is a Knave")
-BSaysBKnave = Symbol("B says he's Knave")
-
-
 # Puzzle 0 
 # A says "I am both a knight and a knave."
 knowledge0 = And(
@@ -25,21 +18,9 @@ knowledge0 = And(
         Implication(AKnight, And(AKnight, AKnave))
 )
 
-# Puzzle 0 
-# A says "I am both a knight and a knave."
-#true_premise = Biconditional(ASaysAKnight, Not(ASaysAKnave))
-#aSays = And(ASaysAKnight, ASaysAKnave)
-#lie = Not(true_premise)
-#knowledge0 = And(
-#        aSays,  
-#        Implication(lie, AKnave)
-#)
-
-
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
-
 knowledge1 = And(
     Biconditional(AKnight, Not(AKnave)), 
     Biconditional(BKnight, Not(BKnave)),
@@ -51,7 +32,12 @@ knowledge1 = And(
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
- 
+    Biconditional(AKnight, Not(AKnave)), 
+    Biconditional(BKnight, Not(BKnave)),
+
+    Implication(AKnight, BKnight), 
+    Implication(BKnight, AKnave),
+    Biconditional(AKnave, BKnight) # entender solução
 )
 
 # Puzzle 3
@@ -69,7 +55,7 @@ def main():
     puzzles = [
         ("Puzzle 0", knowledge0),
         ("Puzzle 1", knowledge1),
-        #("Puzzle 2", knowledge2),
+        ("Puzzle 2", knowledge2),
         #("Puzzle 3", knowledge3)
     ]
     for puzzle, knowledge in puzzles:
